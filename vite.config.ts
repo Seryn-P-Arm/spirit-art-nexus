1f591ea66cf4e96444265ae1efae4a4fd27c2c34
Seryn-P-Arm <serynarmstrong@gmail.com>
2025-10-17 17:12:50 +0200

    npm install and build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/spirit-art-nexus/",
+  
   server: {
     host: "::",
     port: 8080,
